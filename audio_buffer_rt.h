72d17b96e0ac2de6d0dab9dcafabed728347f7a7
Harpo <harpo@harpo.me>
2025-07-02 18:57:53 -0700

    Adds apt install command for prerequisites

--- audio_buffer_rt.h
+++ audio_buffer_rt.h
@@ -7,20 +7,20 @@
 
 /* RT-safe audio buffer management for minimizing file I/O in audio callback */
 
-#define AUDIO_BUFFER_SIZE 8192  /* Large buffer to reduce file I/O frequency */
+#define AUDIO_BUFFER_SIZE 8192 /* Large buffer to reduce file I/O frequency */
 
 struct audio_buffer_rt
 {
-  float *buffer;              /* Audio data buffer */
-  uint32_t size;              /* Total buffer size in samples */
-  uint32_t valid_samples;     /* Number of valid samples currently in buffer */
-  uint32_t read_position;     /* Current read position in buffer */
-  sf_count_t file_position;   /* Position in the source file */
-  bool loop_mode;             /* Whether to loop the audio file */
-  
-  /* Pre-allocated temp buffer for multi-channel reading */
-  float *temp_multichannel;
-  uint32_t temp_size;
+    float *buffer;            /* Audio data buffer */
+    uint32_t size;            /* Total buffer size in samples */
+    uint32_t valid_samples;   /* Number of valid samples currently in buffer */
+    uint32_t read_position;   /* Current read position in buffer */
+    sf_count_t file_position; /* Position in the source file */
+    bool loop_mode;           /* Whether to loop the audio file */
+
+    /* Pre-allocated temp buffer for multi-channel reading */
+    float *temp_multichannel;
+    uint32_t temp_size;
 };
 
 /* Initialize the audio buffer system */
@@ -33,7 +33,7 @@ void audio_buffer_rt_cleanup(struct audio_buffer_rt *ab);
 int audio_buffer_rt_fill(struct audio_buffer_rt *ab, SNDFILE *file, SF_INFO *fileinfo);
 
 /* Read samples from buffer (RT-safe, minimal file I/O) */
-sf_count_t audio_buffer_rt_read(struct audio_buffer_rt *ab, SNDFILE *file, SF_INFO *fileinfo, 
+sf_count_t audio_buffer_rt_read(struct audio_buffer_rt *ab, SNDFILE *file, SF_INFO *fileinfo,
                                 float *output, uint32_t n_samples);
 
 /* Reset buffer to beginning (RT-safe) */
